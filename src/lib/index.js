9ae0d7b51c9c3c2dfc0392a24fa012c3285d36af
Thorsten Hell <thorsten.hell@cismet.de>
2023-06-23 18:00:45 +0200

    added textsnippets for overlays

--- src/lib/index.js
+++ src/lib/index.js
@@ -55,4 +55,58 @@ const getCollabedHelpComponentConfig = ({
   };
 };
 
-export { getCollabedHelpComponentConfig };
+const textElementsForOverlays = {
+  ergebnisse: (
+    <span>
+      Für dieses Gewässer bzw. diesen Bereich liegen auch Ergebnisse aus dem
+      Hochwasserrisikomanagement vor. Die Hochwassergefahrenkarten sind zur
+      Bewertung der Gefährdung heranzuziehen (
+      <a href="https://www.flussgebiete.nrw.de/hochwassergefahrenkarten-und-hochwasserrisikokarten-840">
+        weitergehende Informationen
+      </a>
+      )
+    </span>
+  ),
+  massnahmeVeybach: (
+    <span>
+      In diesem Bereich wurde durch den Erftverband die Hochwasserschutzmaßnahme
+      „Veybachausbau“ umgesetzt. Die umgesetzte Maßnahme konnte noch nicht in
+      dem Modell zur Erstellung der Starkregengefahrenkarten abgebildet werden.
+      Es werden sich daher bei Starkregen abweichende Überflutungsprozesse
+      einstellen. Weiterhin liegen für dieses Gewässer Ergebnisse aus dem
+      Hochwasserrisikomanagement vor, die zu beachten sind.
+    </span>
+  ),
+  massnahmeRotbach: (
+    <span>
+      In diesem Bereich wurde durch den Erftverband die Hochwasserschutzmaßnahme
+      „Rotbach-Umschluss“ umgesetzt. Die umgesetzte Maßnahme konnte noch nicht
+      in dem Modell zur Erstellung der Starkregengefahrenkarten abgebildet
+      werden. Es werden sich daher bei Starkregen abweichende
+      Überflutungsprozesse einstellen. Weiterhin liegen für dieses Gewässer
+      Ergebnisse aus dem Hochwasserrisikomanagement vor, die zu beachten sind.
+    </span>
+  ),
+  stauanlagen: (
+    <span>
+      Zu dieser Anlage bzw. dem Einflussbereich können weitergehende
+      Untersuchungen oder Ergebnisse aus dem Hochwasserrisikomanagement
+      vorliegen, die bei der Bewertung zu berücksichtigen sind. Zudem haben die
+      Vorbedingungen und die Bauwerkssteuerung einen Einfluss auf die
+      Gefährdungssituation. Insbesondere dynamische und ungeplante Vorkommnisse
+      (Verlegung, Verklausung etc.) wie beim Ereignis 2021 können derzeit im
+      Modell nicht ausreichend abgebildet sein.
+    </span>
+  ),
+  stauanlagenSteinbachtalsperre: (
+    <span>
+      Die Steinbachtalsperre wird im Modell gemäß der aktuellen Situation
+      2022/2023 abgebildet. Zukünftige Planungen konnten noch nicht
+      berücksichtigt werden. Das bedeutet, dass es keine Vorfüllung gibt und die
+      Scharte im Damm abgebildet wird. Damit werden sich deutliche Abweichungen
+      im Vergleich zu den bisherigen Erfahrungen ergeben.
+    </span>
+  ),
+};
+
+export { getCollabedHelpComponentConfig, textElementsForOverlays };
